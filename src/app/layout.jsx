28a9c1abfeb292cf6377a4ac3e4ca225e0d318fe
NguyenptViiT <141892463+NguyenptViiT@users.noreply.github.com>
2023-08-14 09:52:52 +0700

    base_nextjs

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,7 +3,7 @@ import './globals.scss';
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-[#f0f0f0] p-[5px] overflow-hidden">{children}</body>
+      <body className="">{children}</body>
     </html>
   );
 }
